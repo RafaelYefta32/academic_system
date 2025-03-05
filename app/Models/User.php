13c14eee4651d455cb6b6149a6a0114265e6fb2a
Rafael Yefta Wijaya <2372037@maranatha.ac.id>
2025-03-05 14:04:02 +0700

    nambah tema dan controller kirim data

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
 
     protected $primaryKey = 'nip';
 
-    public $incrementing = false; // karena primary key bukan auto increment
+    public $incrementing = false; //
 
     /**
      * The attributes that should be hidden for serialization.
